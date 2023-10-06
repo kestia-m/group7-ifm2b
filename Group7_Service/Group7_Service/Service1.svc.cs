59fcfae6ee1fca5bdf7f35be6e5b4f3c0e68b43a
kestia-m <kestiamas@gmail.com>
2023-10-06 20:57:29 +0200

    Update Service1.svc.cs

--- Group7_Service/Group7_Service/Service1.svc.cs
+++ Group7_Service/Group7_Service/Service1.svc.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
+using System.ServiceModel.Web;
 using System.Text;
 
 namespace Group7_Service
@@ -11,8 +12,22 @@ namespace Group7_Service
     // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
     public class Service1 : IService1
     {
-        public void DoWork()
+        public string GetData(int value)
         {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
         }
     }
 }
